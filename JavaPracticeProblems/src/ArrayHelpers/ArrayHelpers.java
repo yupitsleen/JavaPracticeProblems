916060a4313efe1c608358c68af0cfccbdbae839
Eileen Connor <eileenconnorart@gmail.com>
2019-01-19 14:04:39 -0600

    Tidying up comments and removing TODOs.

--- JavaPracticeProblems/src/ArrayHelpers/ArrayHelpers.java
+++ JavaPracticeProblems/src/ArrayHelpers/ArrayHelpers.java
@@ -5,18 +5,11 @@ import java.util.*; // for comparing arrays in main() tests only
 
 class ArrayHelpers {
 
-	/*#1
-	 * minValue returns the minimum value in an array of doubles. You can assume
-	 * the array is nonempty and has no duplicates. Your solution must go
-	 * through the array exactly once. Your solution must not call any other
-	 * functions. Here are some examples (using "==" informally):
-	 * 
-	 * -7 == minValue(new double[] { -7 }) -7 == minValue(new double[] { 1, -4,
-	 * -7, 7, 8, 11 }) -13 == minValue(new double[] { -13, -4, -7, 7, 8, 11 })
-	 * -9 == minValue(new double[] { 1, -4, -7, 7, 8, 11, -9 })
+	/*
+	 * minValue returns the minimum value in an array of doubles. 
 	 */
 	public static double minValue(double[] list) {
-		// TODO
+		
 		double minNum=list[0];
 		for (int i=1; i<list.length; i++) {
 			if (list[i]< minNum) {
@@ -26,21 +19,12 @@ class ArrayHelpers {
 		return minNum;
 	}
 
-	/*#2
+	/*
 	 * minPosition returns the *position of the minimum value in an array of
-	 * doubles. The first position in an array is 0 and the last is the
-	 * array.length-1. You can assume the array is nonempty and has no
-	 * duplicates. Your solution must go through the array exactly once. Your
-	 * solution must not call any other functions. Here are some examples (using
-	 * "==" informally):
-	 * 
-	 * 0 == minPosition(new double[] { -7 })
-	  2 == minPosition(new double[] { 1,-4, -7, 7, 8, 11 }) 
-	  0 == minPosition(new double[] { -13, -4, -7, 7, 8, 11})
-	   6 == minPosition(new double[] { 1, -4, -7, 7, 8, 11, -9 }) */
+	 * doubles.  */
 	 
 	public static int minPosition(double[] list) {
-		 // TODO /
+		
 		double num=list[0];
 		int pos=0;
 		for (int i=1; i<list.length; i++) {
@@ -53,22 +37,12 @@ class ArrayHelpers {
 	
 	}
 
-	/*#3
+	/*
 	 * distanceBetweenMinAndMax returns difference between the minPosition and
 	 * the maxPosition in an array of doubles.
-	 * 
-	 * You can assume the array is nonempty and has no duplicates. **Your solution
-	 * must go through the array exactly once. Your solution must not call any
-	 * other functions. Here are some examples (using "==" informally):
-	 * 
-	 * 0 == distanceBetweenMinAndMax(new double[] { -7 }) // -7,-7 are the min
-	 * and max 3 == distanceBetweenMinAndMax(new double[] { 1, -4, -7, 7, 8, 11
-	 * }), // 7,11 5 == distanceBetweenMinAndMax(new double[] { -13, -4, -7, 7,
-	 * 8, 11 }) // -13,11 1 == distanceBetweenMinAndMax(new double[] { 1, -4,
-	 * -7, 7, 8, 11, -9 }) // -9,11
 	 */
 	public static int distanceBetweenMinAndMax(double[] list) {
-		// TODO
+		
 		double min=list[0];
 		double max=list[0];
 		int posMin=0;
@@ -87,24 +61,11 @@ class ArrayHelpers {
 		return Math.abs(posMax-posMin);
 	}
 
-	/*#4
+	/*
 	 * numUnique returns the number of unique values in an array of doubles.
-	 * Unlike the previous questions, the array may be empty and it may contain
-	 * duplicate values. Also unlike the previous questions, you can assume the
-	 * array is sorted.
-	 * 
-	 * Your solution must go through the array exactly once. Your solution must
-	 * not call any other functions. Here are some examples (using "=="
-	 * informally):
-	 * 
-	 * 0 == numUnique(new double[] { }) 1 == numUnique(new double[] { 11 }) 1 ==
-	 * numUnique(new double[] { 11, 11, 11, 11 }) 8 == numUnique(new double[] {
-	 * 11, 11, 11, 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88, 88 }) 8
-	 * == numUnique(new double[] { 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66,
-	 * 77, 88 })
 	 */
 	public static int numUnique(double[] list) {
-		// TODO
+		
 		if (list.length ==0) { return 0; }
 		
 		int uniqueCnt=1;
@@ -119,24 +80,12 @@ class ArrayHelpers {
 		return uniqueCnt;
 	}
 
-	/*#5
+	/*
 	 * removeDuplicates returns the number of unique values in an array of
-	 * doubles. You may assume that the list is sorted, as you did for
-	 * numUnique.
-	 * 
-	 * Your solution may call numUnique, but should not call any other
-	 * functions. After the call to numUnique, you must go through the array
-	 * exactly one more time. Here are some examples (using "==" informally):
-	 * 
-	 * new double[] { } == removeDuplicates(new double[] { }) new double[] { 11
-	 * } == removeDuplicates(new double[] { 11 }) == removeDuplicates(new
-	 * double[] { 11, 11, 11, 11 }) new double[] { 11, 22, 33, 44, 55, 66, 77,
-	 * 88 } == removeDuplicates(new double[] { 11, 11, 11, 11, 22, 33, 44, 44,
-	 * 44, 44, 44, 55, 55, 66, 77, 88, 88 }) == removeDuplicates(new double[] {
-	 * 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88 })
+	 * doubles.
 	 */
 	public static double[] removeDuplicates(double[] list) {
-		//TODO
+		
 		if (list.length ==0) { return list; }
 		
 		
@@ -166,7 +115,7 @@ class ArrayHelpers {
 		return uniqueNums; 
 	}
 
-	// You can write tests here to make sure your functions work.
+	// Tests.
 	public static void main(String[] args) {
 		// test minValue( double [] )
 		StdOut.println("minValue:");
